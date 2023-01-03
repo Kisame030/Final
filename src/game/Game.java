5afc0985832fc6d73363aaae99cf939fa76a5217
Kisame030 <82775556+Kisame030@users.noreply.github.com>
2023-01-03 22:03:45 +0300

    drop gitignore

--- src/game/Game.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-import player.Player;
-
-public abstract class Game {
-    public abstract void start();
-    public abstract void end();
-    public abstract Player getWinner();
-}
--- src/ui/UserInterface.java
+++
@@ -1,7 +0,0 @@
-package ui;
-
-public abstract class UserInterface {
-    public abstract void displayMessage(String message);
-    public abstract int getInput();
-}
-
